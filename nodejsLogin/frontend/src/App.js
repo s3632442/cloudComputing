8fd3803d63251742e16105aa23ad2b28b2a97f33
s3632442 <s363244@student.rmit.edu.au>
2022-10-19 21:43:17 +1100

    skeleton working

---
+++ nodejsLogin/frontend/src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Register from './Register';
+import Login from './Login';
+import PremiumContent from './PremiumContent';
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <div className='header'>
+          <NavLink exact activeClassName='active' to='/'>
+            Home
+          </NavLink>
+          <NavLink exact activeClassName='active' to='/register'>
+            Register
+          </NavLink>
+          <NavLink exact activeClassName='active' to='/login'>
+            Login
+          </NavLink>
+          <NavLink exact activeClassName='active' to='/premium-content'>
+            Premium Content
+          </NavLink>
+          <div className='content'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/premium-content' component={PremiumContent} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ nodejsLogin/frontend/src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            This is the Login page!
+        </div>
+    )
+}
+export default Login;
